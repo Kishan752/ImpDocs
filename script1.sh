6d1c717a4ed72484fd26469cb9a49b5fa66c6bb6
Kishan Kumar <86073675+Kishan752@users.noreply.github.com>
2021-10-11 18:43:43 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,13 +1,13 @@
 printf "Step 1:Setting up Temp variables";
-export DEV_UN=kishan75289@pwc.com;
-export consumer_key=3MVG9fe4g9fhX0E4O2xNyNuXD0qDJoUumlelZBzYyVutz8UNH9T.ifxEcbscvqM7jkgajQmSbRzESFbZBIyXS
-export key_location="D:\Lightning Codes\AutomatedDevelopments\Assets\server.key"
-export project_path="D:\Lightning Codes\AutomatedDevelopments\Projects"
+export DEV_UN=your_user_name_in_the_Dev_hub_org
+export consumer_key=Your_consumerkey_from_connected_App
+export key_location="D:\Lightning Codes\AutomatedDevelopments\Assets\server.key" # path to the key file created as part of openssl self signed certificate
+export project_path="D:\Lightning Codes\AutomatedDevelopments\Projects" # path where you would clone the repo and create scrath orgs
 
 printf "Step 2:Moving in Project folder"
 
 cd "$project_path"
-pwd
+
 
 printf "Step 3:creating new Directory $(date +'%Y%m%d')\n";
 
@@ -15,13 +15,11 @@ mkdir  $(date +'%Y%m%d')
 
 cd  $(date +'%Y%m%d')
 
-pwd
-
 printf "Step 4:cloning github";
 
-git clone https://github.com/Kishan752/tree-plantation.git
+git clone https://github.com/Kishan752/SamplelwcStructure.git  # replcae with your own Github repo or you can use this one also for testing
 
-cd tree-plantation
+cd SamplelwcStructure
 
 printf "Step 5:authorizing Dev Hub";
 
